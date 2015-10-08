cf8f2a96b46d17daed5607b0cfcb98348d9a12a4
matrix207 <dennis.cpp@gmail.com>
2015-10-08 13:23:17 +0800

    add files

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp -rf matrix207-vim ~/.matrix207-vim
+
+cd ~
+
+ln -s ~/.matrix207-vim/.vimrc
+ln -s ~/.matrix207-vim/.vimrc.before
+ln -s ~/.matrix207-vim/.vimrc.bundles
+
+mkdir ~/.vim
+tar xvf ~/.matrix207-vim/bundle.tar.gz -C ~/.vim/
+
